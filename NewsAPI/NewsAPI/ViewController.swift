9f036d3648f0d87c7b0601ddbb0901d442d47c44
Frederico Novack <fredericonovack@Fredericos-MacBook-Pro.local>
2022-10-14 17:43:06 -0300

    Initial Commit

---
+++ NewsAPI/NewsAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsAPI
+//
+//  Created by Frederico Novack on 10/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
